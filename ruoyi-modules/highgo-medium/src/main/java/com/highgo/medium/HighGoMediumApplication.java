c068036fc11027a0444331395a9093e53bd43d34
eszhang <zhangershuai@highgo.com>
2022-11-22 12:29:24 +0800

    添加介质平台后台模块medium

--- ruoyi-modules/highgo-medium/src/main/java/com/highgo/medium/HighGoMediumApplication.java
+++ ruoyi-modules/highgo-medium/src/main/java/com/highgo/medium/HighGoMediumApplication.java
@@ -1,24 +1,23 @@
-package com.highgo.crm;
+package com.highgo.medium;
 
 import com.ruoyi.common.security.annotation.EnableCustomConfig;
 import com.ruoyi.common.security.annotation.EnableRyFeignClients;
 import com.ruoyi.common.swagger.annotation.EnableCustomSwagger2;
-import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * CRM系统模块
+ * 介质模块
  */
 @EnableCustomConfig
 @EnableCustomSwagger2
 @EnableRyFeignClients
 @SpringBootApplication
 @MapperScan("com.highgo.**.mapper")
-public class HighGoCRMApplication {
+public class HighGoMediumApplication {
     public static void main(String[] args) {
-        SpringApplication.run(HighGoCRMApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  CRM模块启动成功   ლ(´ڡ`ლ)ﾞ");
+        SpringApplication.run(HighGoMediumApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  介质模块启动成功   ლ(´ڡ`ლ)ﾞ");
     }
 }
