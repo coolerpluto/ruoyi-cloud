c64b97034dc1b290e28db19c3a1076ebd948a07a
esz_pc <Eszhanga@163.com>
2023-03-28 17:22:06 +0800

    商机-完善运营商相关信息查询

--- ruoyi-modules/highgo-crm/src/main/java/com/highgo/crm/domain/OpportunitySoftwareOperation.java
+++ ruoyi-modules/highgo-crm/src/main/java/com/highgo/crm/domain/OpportunitySoftwareOperation.java
@@ -7,7 +7,7 @@ import com.ruoyi.common.core.web.domain.BaseEntity;
 
 /**
  * 商机应用运营信息对象 t_opportunity_software_operation
- * 
+ *
  * @author eszhang
  * @date 2023-02-22
  */
@@ -15,138 +15,210 @@ public class OpportunitySoftwareOperation extends BaseEntity
 {
     private static final long serialVersionUID = 1L;
 
-    /** 主键 */
+    /**
+     * 主键
+     */
     private Long id;
 
-    /** 商机Code */
+    /**
+     * 商机Code
+     */
     @Excel(name = "商机Code")
     private String opportunityCode;
 
-    /** 应用软件Id */
+    /**
+     * 应用软件Id
+     */
     private Long applicationId;
 
-    /** 应用软件名字 */
+    /**
+     * 应用软件名字
+     */
     @Excel(name = "应用软件名字")
     private String applicationName;
 
-    /** 是否需要适配 */
+    /**
+     * 是否需要适配
+     */
     @Excel(name = "是否需要适配")
     private String isvNeedAdapted;
 
-    /** 招标前目标进度 */
+    /**
+     * 招标前目标进度
+     */
     @Excel(name = "招标前目标进度")
     private String targetAdaptedProgress;
 
-    /** 当前适配进度 */
+    /**
+     * 当前适配进度
+     */
     @Excel(name = "当前适配进度")
     private String currentAdaptedProgress;
 
-    /** 运作主体Id */
+    /**
+     * 运作主体Id
+     */
     private Long operationalId;
 
-    /** 运作主体名字 */
+    /**
+     * 运作主体名字
+     */
     @Excel(name = "运作主体名字")
     private String operationalName;
 
-    public void setId(Long id) 
+    /**
+     * 系统开发商ID
+     */
+    private String isvId;
+
+    /**
+     * 系统开发商
+     */
+    private String isv;
+
+    /**
+     * 软件分类
+     */
+    private String category;
+
+    public String getIsvId()
+    {
+        return isvId;
+    }
+
+    public void setIsvId(String isvId)
+    {
+        this.isvId = isvId;
+    }
+
+    public String getIsv()
+    {
+        return isv;
+    }
+
+    public void setIsv(String isv)
+    {
+        this.isv = isv;
+    }
+
+    public String getCategory()
+    {
+        return category;
+    }
+
+    public void setCategory(String category)
+    {
+        this.category = category;
+    }
+
+    public void setId(Long id)
     {
         this.id = id;
     }
 
-    public Long getId() 
+    public Long getId()
     {
         return id;
     }
-    public void setOpportunityCode(String opportunityCode) 
+
+    public void setOpportunityCode(String opportunityCode)
     {
         this.opportunityCode = opportunityCode;
     }
 
-    public String getOpportunityCode() 
+    public String getOpportunityCode()
     {
         return opportunityCode;
     }
-    public void setApplicationId(Long applicationId) 
+
+    public void setApplicationId(Long applicationId)
     {
         this.applicationId = applicationId;
     }
 
-    public Long getApplicationId() 
+    public Long getApplicationId()
     {
         return applicationId;
     }
-    public void setApplicationName(String applicationName) 
+
+    public void setApplicationName(String applicationName)
     {
         this.applicationName = applicationName;
     }
 
-    public String getApplicationName() 
+    public String getApplicationName()
     {
         return applicationName;
     }
-    public void setIsvNeedAdapted(String isvNeedAdapted) 
+
+    public void setIsvNeedAdapted(String isvNeedAdapted)
     {
         this.isvNeedAdapted = isvNeedAdapted;
     }
 
-    public String getIsvNeedAdapted() 
+    public String getIsvNeedAdapted()
     {
         return isvNeedAdapted;
     }
-    public void setTargetAdaptedProgress(String targetAdaptedProgress) 
+
+    public void setTargetAdaptedProgress(String targetAdaptedProgress)
     {
         this.targetAdaptedProgress = targetAdaptedProgress;
     }
 
-    public String getTargetAdaptedProgress() 
+    public String getTargetAdaptedProgress()
     {
         return targetAdaptedProgress;
     }
-    public void setCurrentAdaptedProgress(String currentAdaptedProgress) 
+
+    public void setCurrentAdaptedProgress(String currentAdaptedProgress)
     {
         this.currentAdaptedProgress = currentAdaptedProgress;
     }
 
-    public String getCurrentAdaptedProgress() 
+    public String getCurrentAdaptedProgress()
     {
         return currentAdaptedProgress;
     }
-    public void setOperationalId(Long operationalId) 
+
+    public void setOperationalId(Long operationalId)
     {
         this.operationalId = operationalId;
     }
 
-    public Long getOperationalId() 
+    public Long getOperationalId()
     {
         return operationalId;
     }
-    public void setOperationalName(String operationalName) 
+
+    public void setOperationalName(String operationalName)
     {
         this.operationalName = operationalName;
     }
 
-    public String getOperationalName() 
+    public String getOperationalName()
     {
         return operationalName;
     }
 
     @Override
-    public String toString() {
-        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-            .append("id", getId())
-            .append("opportunityCode", getOpportunityCode())
-            .append("applicationId", getApplicationId())
-            .append("applicationName", getApplicationName())
-            .append("isvNeedAdapted", getIsvNeedAdapted())
-            .append("targetAdaptedProgress", getTargetAdaptedProgress())
-            .append("currentAdaptedProgress", getCurrentAdaptedProgress())
-            .append("operationalId", getOperationalId())
-            .append("operationalName", getOperationalName())
-            .append("createBy", getCreateBy())
-            .append("createTime", getCreateTime())
-            .append("updateBy", getUpdateBy())
-            .append("updateTime", getUpdateTime())
-            .append("remark", getRemark())
-            .toString();
+    public String toString()
+    {
+        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+                .append("id", getId())
+                .append("opportunityCode", getOpportunityCode())
+                .append("applicationId", getApplicationId())
+                .append("applicationName", getApplicationName())
+                .append("isvNeedAdapted", getIsvNeedAdapted())
+                .append("targetAdaptedProgress", getTargetAdaptedProgress())
+                .append("currentAdaptedProgress", getCurrentAdaptedProgress())
+                .append("operationalId", getOperationalId())
+                .append("operationalName", getOperationalName())
+                .append("createBy", getCreateBy())
+                .append("createTime", getCreateTime())
+                .append("updateBy", getUpdateBy())
+                .append("updateTime", getUpdateTime())
+                .append("remark", getRemark())
+                .toString();
     }
 }
