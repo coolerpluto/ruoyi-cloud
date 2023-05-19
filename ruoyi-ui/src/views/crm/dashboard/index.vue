b22f839ae2064161a34985b6038efc9fd9b3c210
esz_pc <Eszhanga@163.com>
2023-05-19 13:40:47 +0800

    add dashboard background images

--- ruoyi-ui/src/views/crm/dashboard/index.vue
+++ ruoyi-ui/src/views/crm/dashboard/index.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="app-container">
-看板
-
+  <div class="app-container ">
+    <!--    <div class="scan"></div>-->
+    <div class="dashboard">
+      <img class="imgaction" src="@/assets/images/scan.png">
+    </div>
+    <div style="color: white;">
+      内容写这里
+    </div>
   </div>
 </template>
 
@@ -30,3 +35,53 @@ export default {
   },
 };
 </script>
+
+<style rel="stylesheet/scss" lang="scss">
+.dashboard {
+  background-image: url("../../../assets/images/bg18.jpg");
+  background-size: cover;
+  //background-position: center;
+  background-position-y: -30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: -99;
+}
+
+.scan {
+  background-image: url("../../../assets/images/scan.png");
+  background-size: cover;
+  background-position: center;
+  animation-duration: 5s;
+  animation-name: run;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+  opacity: 1;
+  width: 663px;
+  height: 663px;
+  position: absolute;
+}
+
+.imgaction {
+  animation-duration: 5s;
+  animation-name: run;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+  opacity: 1;
+  width: 663px;
+  height: 663px;
+  position: absolute;
+}
+
+@keyframes run {
+  from {
+    transform: rotateZ(0);
+  }
+  to {
+    transform: rotateZ(360deg);
+  }
+}
+</style>
