ee2991a4ddef5987887aea8a3aa41cfabc59169c
esz_pc <Eszhanga@163.com>
2024-03-04 12:53:24 +0800

    add water mark

--- ruoyi-ui/src/App.vue
+++ ruoyi-ui/src/App.vue
@@ -7,10 +7,30 @@
 
 <script>
 import ThemePicker from "@/components/ThemePicker";
+import watermark from "watermark-dom";
 
 export default {
   name: "App",
   components: { ThemePicker },
+  mounted() {
+    // 加载水印
+    let _this=this
+    setTimeout(function () {
+      const username = _this.$store.state.user.name;
+      watermark.load({
+        watermark_txt: username +"@"+ new Date().toLocaleString() ,
+        watermark_x:140,                     //水印起始位置x轴坐标
+        watermark_y:150,                     //水印起始位置Y轴坐标
+        watermark_x_space:100,              //水印x轴间隔
+        watermark_y_space:50,               //水印y轴间隔
+        watermark_fontsize:'20px',
+        watermark_alpha:0.15,
+        watermark_angle:30,
+        watermark_width:300,
+        watermark_height:300,
+      });
+    }, 2000);
+  },
   metaInfo() {
     return {
       title: this.$store.state.settings.dynamicTitle && this.$store.state.settings.title,
