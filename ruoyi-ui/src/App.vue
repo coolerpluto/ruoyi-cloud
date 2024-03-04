73c6c16cad23adbc7e46a00a4729c4afbe26895d
esz_pc <Eszhanga@163.com>
2024-03-04 16:26:03 +0800

    change timeout to interval

--- ruoyi-ui/src/App.vue
+++ ruoyi-ui/src/App.vue
@@ -15,21 +15,27 @@ export default {
   mounted() {
     // 加载水印
     let _this=this
-    setTimeout(function () {
-      const username = _this.$store.state.user.name;
-      watermark.load({
-        watermark_txt: username +"@"+ new Date().toLocaleString() ,
-        watermark_x:140,                     //水印起始位置x轴坐标
-        watermark_y:150,                     //水印起始位置Y轴坐标
-        watermark_x_space:100,              //水印x轴间隔
-        watermark_y_space:50,               //水印y轴间隔
-        watermark_fontsize:'20px',
-        watermark_alpha:0.15,
-        watermark_angle:30,
-        watermark_width:300,
-        watermark_height:300,
-      });
-    }, 2000);
+    this.timerWatermark = setInterval(function () {
+        const username = _this.$store.state.user.name;
+        watermark.load({
+          watermark_txt: username +"@"+ new Date().toLocaleString() ,
+          watermark_x:140,                     //水印起始位置x轴坐标
+          watermark_y:150,                     //水印起始位置Y轴坐标
+          watermark_x_space:100,              //水印x轴间隔
+          watermark_y_space:50,               //水印y轴间隔
+          watermark_fontsize:'20px',
+          watermark_alpha:0.15,
+          watermark_angle:30,
+          watermark_width:300,
+          watermark_height:300,
+        });
+    })
+  },
+  // 销毁时清除计时器
+  beforeDestroy: function () {
+    if (this.timerWatermark) {
+      clearInterval(this.timerWatermark)
+    }
   },
   metaInfo() {
     return {
